6210f3f2bb75f81f8be397640b7dd76ba007a851
hudashoeib <hudashoeib@gmail.com>
2025-12-18 13:32:04 +0200

    ok

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,21 +1,29 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const Create = () => {
   return (
-    <Box>
-      <Box component="main" className="container">
-        <Typography variant="body1" color="primary">
-          this is create page
-        </Typography>
-        <Typography variant="body1" color="primary">
-          this is create page Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Veniam ut molestiae quas et optio inventore corporis,
-          necessitatibus dolores impedit a, quaerat ullam, assumenda culpa
-          consequuntur voluptate reiciendis eaque. Exercitationem, corporis.
-        </Typography>
+    <>
+      <Helmet>
+        <title>Create </title>
+        <meta
+          name="description"
+          content="This is the Create page to craete ur default task"
+        />
+      </Helmet>
+      <Box>
+        <Box component="main" className="container">
+          <Typography variant="body1" color="primary">
+            this is create page
+          </Typography>
+          <Typography variant="body1" color="primary">
+            this is create page Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Veniam ut molestiae quas et optio inventore
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,26 @@
 import { Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   return (
-    <Box className="parent">
-      <Box component="main" className="container">
-        <Toolbar />
-        <Typography>
-          consequuntur dignissimos numquam at nisi porro a, quaerat rem
-          repellendus. Voluptates perspiciatis, in pariatur impedit, nam facilis
-          libero dolorem dolores sunt inventore perferendis, aut sapiente modi
-          nesciunt.
-        </Typography>
+    <>
+      <Helmet>
+        <title> Home page </title>
+        <meta name="description" content="This is the Home page" />
+      </Helmet>
+      <Box className="parent">
+        <Box component="main" className="container">
+          <Toolbar />
+          <Typography>
+            consequuntur dignissimos numquam at nisi porro a, quaerat rem
+            repellendus. Voluptates perspiciatis, in pariatur impedit, nam
+            facilis libero dolorem dolores sunt inventore perferendis, aut
+            sapiente modi nesciunt.
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
